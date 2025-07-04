7e250ec382ddcb44407f693bfda09039461f0d5d
Mike <mikealexx97@gmail.com>
2025-07-04 13:12:47 +0300

    round

--- lb.py
+++ lb.py
@@ -10,41 +10,56 @@ BACKEND_SERVERS = [
     ('192.168.0.102', 80),
     ('192.168.0.103', 80)
 ]
+# =====================
 
 # === Round-robin index ===
 next_server_index = 0
 server_lock = threading.Lock()
+# =========================
+
+# === Backend Server Sockets ===
+server1_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server1_socket.connect(BACKEND_SERVERS[0])
+
+server2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server2_socket.connect(BACKEND_SERVERS[1])
+
+server3_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server3_socket.connect(BACKEND_SERVERS[2])
+
+backend_sockets = [
+    server1_socket,
+    server2_socket,
+    server3_socket
+]
+# =============================
 
 
 def choose_next_server():
     global next_server_index
     server_lock.acquire()
-    server = BACKEND_SERVERS[next_server_index]
+    server_index = next_server_index
     next_server_index = (next_server_index + 1) % len(BACKEND_SERVERS)
     server_lock.release()
-    return server
+    return server_index
 
 
-def handle_client(client_socket):
+def handle_client(client_socket: socket.socket):
     try:
         request_data = client_socket.recv(1024)
         if not request_data:
             client_socket.close()
             return
 
-        backend_host, backend_port = choose_next_server()
+        server_index = choose_next_server()
 
-        backend_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        backend_socket.connect((backend_host, backend_port))
-        backend_socket.sendall(request_data)
+        backend_sockets[server_index].sendall(request_data)
 
         while True:
-            response = backend_socket.recv(4096)
+            response = backend_sockets[server_index].recv(1024)
             if not response:
                 break
             client_socket.sendall(response)
-
-        backend_socket.close()
 
     except Exception as e:
         print("[ERROR] {}".format(e))
@@ -54,24 +69,25 @@ def handle_client(client_socket):
 
 
 def start_load_balancer():
-    print("[LB] Starting Load Balancer on {}:{}".format(LB_HOST, LB_PORT))
+    print("Starting Load Balancer on {}:{}".format(LB_HOST, LB_PORT))
 
     lb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    lb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    lb_socket.bind((LB_HOST, LB_PORT))
-    lb_socket.listen(20)
+    lb_socket.bind((LB_HOST))
+    lb_socket.listen()
 
     try:
         while True:
             client_socket, addr = lb_socket.accept()
-            print("[LB] Connection from {}".format(addr))
-            t = threading.Thread(target=handle_client, args=(client_socket,))
+            print("Connection from {}:{}".format(addr[0], addr[1]))
+            t = threading.Thread(target=handle_client, args=(client_socket))
             t.daemon = True
             t.start()
     except KeyboardInterrupt:
-        print("[LB] Shutting down.")
+        print("Shutting down.")
     finally:
         lb_socket.close()
+        for sock in backend_sockets:
+            sock.close()
 
 
 if __name__ == "__main__":
